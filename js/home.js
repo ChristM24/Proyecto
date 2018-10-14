1aeefdcbe05a2e2439418e9807e92ae23764d22e
ChristM24 <chriz_jona.10@hotmail.com>
2018-10-14 10:02:27 -0500

    Drinks finalizado

--- js/home.js
+++ js/home.js
@@ -1,5 +1,14 @@
-
-
+$(document).ready(function(){
+        $('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+});
 //-----Boton Responsive----//
 $('.boton-responsive').click(function () {
                 $('.boton-responsive').toggleClass('activo');
